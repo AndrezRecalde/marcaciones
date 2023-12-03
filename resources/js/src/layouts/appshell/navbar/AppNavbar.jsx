ce2fa1e8ccada98a44f2e10653d7d19edd35a203
CristhianRec <azw1021@gmail.com>
2023-12-02 19:18:23 -0500

    id tthh

--- resources/js/src/layouts/appshell/navbar/AppNavbar.jsx
+++ resources/js/src/layouts/appshell/navbar/AppNavbar.jsx
@@ -5,7 +5,7 @@ import classes from "./NavbarModule/AppNavbar.module.css";
 
 export const AppNavbar = ({ toggleMobile }) => {
     const srv_user = JSON.parse(localStorage.getItem("user_srvm"));
-    const mockdata =  srv_user.cdgo_usrio === 1303 ? lFuncionarioTTHH : lFuncionarios;
+    const mockdata =  srv_user.cdgo_usrio === 1303 || srv_user.cdgo_usrio === 208 ? lFuncionarioTTHH : lFuncionarios;
 
     const links = mockdata.map((item) => (
         <LinksGroup {...item} key={item.label} toggleMobile={toggleMobile} />
