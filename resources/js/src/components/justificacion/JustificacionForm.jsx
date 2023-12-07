c8ba85db0293760507a3d3e7887b552e76262b4a
CristhianRec <azw1021@gmail.com>
2023-12-07 01:59:58 -0500

    change_2

--- resources/js/src/components/justificacion/JustificacionForm.jsx
+++ resources/js/src/components/justificacion/JustificacionForm.jsx
@@ -96,7 +96,7 @@ export const JustificacionForm = ({ disabled, form }) => {
         </ActionIcon>
     );
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         startAddJustificacion(
             dayjs(fecha_inicio).format("YYYY-MM-DD"),
@@ -109,7 +109,7 @@ export const JustificacionForm = ({ disabled, form }) => {
         );
         if (storageFields !== null) {
             const { fecha_inicio:fecha_i, fecha_fin:fecha_f, cdgo_dprtmnto: dept, cdgo_usrio: ur } = storageFields;
-            await startLoadMarcacionesAdmin(
+            startLoadMarcacionesAdmin(
                 srv_user.id_empresa,
                 null,
                 dayjs(fecha_i).format("YYYY-MM-DD"),
